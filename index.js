ba1b6969b29e02c315cf3df8d2202a5d4518a5d7
RowanKinross <rowankinross@yahoo.co.uk>
2024-02-12 17:44:57 +0000

    styling_added_and_validation_added_on_inquiries

--- index.js
+++ index.js
@@ -11,11 +11,33 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./src/page-template.js");
 const Employee = require("./lib/Employee.js");
 
+const employeeArr = [];
 
 
 
 
-//function for team manager to sign in
+// validation to check if email input exists and is in the right format:
+const emailValidation = (email) => {
+  valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+  if (valid) {
+      return true;
+  } else {
+      return 'Please enter a valid email'
+  }
+}
+// validation to check if input exists:
+const inputValidation = (input) => {
+  if (!input) {
+      return 'input cannot be blank.'
+  } else {
+      return true;
+  }
+}
+
+
+
+
+//function for team manager to sign in / creating team manager object:
 function teamManager(){
   return inquirer
   .prompt ([
@@ -23,82 +45,104 @@ function teamManager(){
       type: "input",
       name: "name",
       message: "Welcome back team manager. Please enter your name:",
+      validate: inputValidation
     },
     { // Employee ID
       type: "input",
       name: "id",
-      message: "Employee ID:"
+      message: "Employee ID:",
+      validate: inputValidation 
     },
     { // Email address
       type: "input",
       name: "email",
-      message: "Email:"
+      message: "Email:",
+      validate: emailValidation
     },
     { // Office number
       type: "input",
       name: "officeNumber",
-      message: "Office Number:"
+      message: "Office Number:",
+      validate: inputValidation
     }
   ])
 }
 
+
+
+// function for creating an engineer object:
 function engineerQ() {
   return inquirer
   .prompt([
     { // Engineer's Name
     type: "input",
     name: "name",
-    message: "Name:"
+    message: "Name:",
+    validate: inputValidation
     },
     { // ID
     type: "input",
     name: "id",
-    message: "ID:"
+    message: "ID:",
+    validate: inputValidation 
     },
     { // Email  
     type: "input",
     name: "email",
-    message: "Email:"
+    message: "Email:",
+    validate: emailValidation
     },
     { // GitHub username
     type: "input",
     name: "github",
-    message: "GitHub Username:"
+    message: "GitHub Username:",
+    validate: inputValidation
     }
   ])
 }
 
+
+
+
+// function for creating an intern object:
 function internQ() {
   return inquirer
   .prompt([
     { // Intern’s name
     type: "input",
     name: "name",
-    message: "Name:"
+    message: "Name:",
+    validate: inputValidation
     },
     { // ID
     type: "input",
     name: "id",
-    message: "ID:"
+    message: "ID:",
+    validate: inputValidation 
     },
     { // Email  
     type: "input",
     name: "email",
-    message: "Email:"
+    message: "Email:",
+    validate: emailValidation
     },
     { // School
     type: "input",
     name: "school",
-    message: "School:"
+    message: "School:",
+    validate: inputValidation
     }
   ])
 }
 
+
+
+// function for giving menu options:
 function menuBar() {
   return inquirer
   .prompt([
     {
-    type: "rawlist",
+    type: "list",
     name: "menuBarOpt",
     message: "what would you like to do:",
     choices: ['Add an engineer', 'Add an intern', 'Finish building the team'],
@@ -110,6 +154,8 @@ function menuBar() {
           console.log('Adding an engineer...');
           engineerQ().then(answers => {
               console.log(`Engineer: ${answers.name} was added`);
+              let newEngineer = new Engineer(answers.name, answers.id, answers.email, answers.github)
+              employeeArr.push(newEngineer)
               menuBar(); // Return to menu after completing engineer input
           });
           break;
@@ -117,24 +163,30 @@ function menuBar() {
           console.log('Adding an intern...');
           internQ().then(answers => {
               console.log(`Intern: ${answers.name} was added`);
+              let newIntern = new Intern(answers.name, answers.id, answers.email, answers.school)
+              employeeArr.push(newIntern)
               menuBar(); // Return to menu after completing intern input
           });
           break;
       case 'Finish building the team':
           console.log('Finishing building the team...');
-          // Add finish building the team sequence
-            //print to html
-            //close inquirer
-          break;
+          //print to html
+          const data = render(employeeArr)
+          fs.writeFileSync(outputPath, data)
+          break; //close inquirer
   }
   })
 }
 
-function startApplication() { // once application open in node
-  teamManager().then(() => { // ask for the manager's details
+
+// function to start the application:
+function startApplication() { // once application is open in node,
+  teamManager().then((response) => { // 1. ask for the manager's details
+    const teamManager = new Manager(response.name, response.id, response.email, response.officeNumber)
+    employeeArr.push(teamManager) // 2. push manager to employee array
     console.log(`manager login successful`)
-    menuBar() // ask what the manager would like to do & proceed with what follows that choice
-  }).catch(error => { // log an error if problem with manager logging in
+    menuBar() // 3. Ask what the manager would like to do & proceed with what follows that choice (either create employee cards or complete the team's html page)
+  }).catch(error => { // log an error if problem with manager's input
     console.error('Login failed:', error);
   });
 }
